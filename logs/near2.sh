19f94ac38fb92a9181d4a7b59e4df676c721c244
0xevoid <0xevoid.eth@gmail.com>
2024-04-08 23:45:39 +0700

    Create near2.sh

---
+++ logs/near2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+RPC_URL="https://near.lava.build/lava-referer-e52ee6f5-57e0-42c7-bd01-ba3a973ad7f2/"
+
+OUTPUT_DIR="logs"
+
+near_data=$(curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"block","id":"HappyCuanAirdrop","params":{"finality":"final"}}' "$RPC_URL")
+
+if [ $? -eq 0 ]; then
+    if [ ! -d "$OUTPUT_DIR" ]; then
+        mkdir "$OUTPUT_DIR"
+    fi
+    
+    echo "$near_data" >> "$OUTPUT_DIR/near.txt"
+    
+    echo "NEAR data fetched and saved to $OUTPUT_DIR/near.txt."
+else
+    echo "Failed to fetch NEAR data."
+fi
