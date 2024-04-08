8b371afe969a6e199bad28c164babe337ff3e84b
0xevoid <0xevoid.eth@gmail.com>
2024-04-08 23:18:50 +0700

    Update ethereum.sh

--- ethereum.sh
+++ ethereum.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://eth1.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://eth1.lava.build/lava-referer-3b4a26df-b40a-4244-be5c-9243577a7794/"
 
 OUTPUT_DIR="logs"
 
