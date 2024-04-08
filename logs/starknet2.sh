ec0566dbda9c38ef11bacec01b9f845f0444990a
0xevoid <0xevoid.eth@gmail.com>
2024-04-08 23:46:15 +0700

    Create starknet2.sh

---
+++ logs/starknet2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+RPC_URL="https://rpc.starknet.lava.build/lava-referer-e52ee6f5-57e0-42c7-bd01-ba3a973ad7f2/"
+
+OUTPUT_DIR="logs"
+
+starknet_data=$(curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","id":"1","method":"starknet_blockNumber"}' "$RPC_URL")
+
+if [ $? -eq 0 ]; then
+    if [ ! -d "$OUTPUT_DIR" ]; then
+        mkdir "$OUTPUT_DIR"
+    fi
+    
+    echo "$starknet_data" >> "$OUTPUT_DIR/starknet.txt"
+    
+    echo "StarkNet data fetched and saved to $OUTPUT_DIR/starknet.txt."
+else
+    echo "Failed to fetch StarkNet data."
+fi
